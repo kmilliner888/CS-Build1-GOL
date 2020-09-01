50ecb09899f7887bde98d74a4f2d2a06f5920e4c
Kaylen Milliner <kaylen.milliner@gmail.com>
2020-08-31 22:12:03 -0400

    Updates

---
+++ precache-manifest.f97696b8ca6e58d6a6922a7351a129ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4b109a93b7430961968ec096c71819b",
+    "url": "/%3Cgithub-repo-name%3E/index.html"
+  },
+  {
+    "revision": "47ab1af1080292f605c0",
+    "url": "/%3Cgithub-repo-name%3E/static/css/main.07a65733.chunk.css"
+  },
+  {
+    "revision": "5f0beb6ebb20ce803ef0",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.ce15d6ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%3Cgithub-repo-name%3E/static/js/2.ce15d6ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ab1af1080292f605c0",
+    "url": "/%3Cgithub-repo-name%3E/static/js/main.9dfedcb7.chunk.js"
+  },
+  {
+    "revision": "75be2a7f44a4408b4ec1",
+    "url": "/%3Cgithub-repo-name%3E/static/js/runtime-main.ac9945e6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%3Cgithub-repo-name%3E/static/media/logo.5d5d9eef.svg"
+  }
+]);
